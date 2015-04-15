fc85f5ee2d9ce9e96858d7cb5143feaba85aac88
J.R. Leeman <kd5wxb@gmail.com>
2015-04-14 22:05:55 -0400

    Clean, fix leap year, function for new file names

--- Tilt_Logger_Hourly/Tilt_Logger_Hourly.ino
+++ Tilt_Logger_Hourly/Tilt_Logger_Hourly.ino
@@ -8,10 +8,6 @@
         We use the SD card on that shield as well, just because of the way things
         mount in the housing. See the repository readme for more hardware description.
 
-	The circuit:
-	* list the components attached to each input
-	* list the components attached to each output
-
 	Created 14 04 2015
 	By John R. Leeman
 	Modified 14 04 2015
@@ -21,14 +17,22 @@
 
 */
 
+// DEV NOTES:
+// - Master works and is logging as of about noon April 14
+// - This code has not been tested, but shouldn't cause any 
+//   major issues. Maybe just by putting the file logger
+//   new file in a function we could break.Maybe I need to figure out
+//   how to return the file object? Testing will tell. 
+// - The GPS setup stuff should be tested, it didn't seem to work
+//   a few nights ago when I was playing with it.
+
 #include <WildFire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <TinyGPS.h>
 
-uint8_t index = 0;
-// Holds current hour, 99 forces us to start a new log at first GPS lock
-int current_hour = 99; 
+uint8_t index = 0; // Buffer index
+int current_hour = 99; // 99 forces us to start a new log at first GPS lock
 char tiltBuffer[100];
 boolean writeTilt = false; // State variable for when we are ready to log
 
@@ -52,7 +56,15 @@ void setup() {
   bytesSent += Serial.write("*9900XYC2\n"); // Set to 1 Hz output
   delay(5000);
   
-  // TODO: Add GPS setup verification here
+  // Setup the GPS Parameters
+  Serial1.println("$PMTK251,9600*17"); // 9600 Baud
+  delay(1000);
+  Serial1.println("$PMTK220,1000*1F"); // 1Hz Update
+  delay(1000);
+  Serial1.println("$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"); // GPRMC and GGA
+  delay(1000);
+  Serial1.println("$PGCMD,33,0*6D"); // No Antenna State Updates Please
+  delay(1000);
   
   // Setup and start the SD card
   pinMode(10, OUTPUT);
@@ -99,8 +111,10 @@ void loop() {
   // Done with getting any tilt meter data!
   
   
-  // Log the tiltmeter data if we are ready
+  // Log the data if we are ready
   if (writeTilt){
+    // Grab the latest gps data and parse it up for writing to the log file,
+    // this should be the most recent time stamp at the time of the tilt reading.
     int year;
     byte month, day, hour, minute, second, hundredths;
     float flat, flon;
@@ -110,28 +124,10 @@ void loop() {
     
     // Check and deal with the need for a new logging file
     if (hour != current_hour){
-      logfile.close();
-      char name[13];
-      getLogFileName(name,day,month,year,hour);
-
-      // Prevent over-writing by adding to the sub-sequence set
-      for (uint8_t i = 0; i < 10; i++) {
-        name[7] = '0' + i;
-        if (! SD.exists(name)) {
-          break;
-        }
-      }
-
-      // Open the new file and set the current hour to the new hour
-      logfile = SD.open(name, FILE_WRITE);
-      if( ! logfile ) {
-        //error(1);
-        int dummy = 0;
-      }
-      current_hour = hour;
-      logfile.write("YYYY-MM-DDTHH:MM:SS,NUM_SATELLITES,LATITUDE,LONGITUDE,ALTITUDE,X_TILT,Y_TILT,TEMPERATURE,SERIAL_NUMBER\n");
+      start_new_logfile(day, month, year, hour);
     }
     
+    // Format and write out the GPS data
     char sz[32];
     sprintf(sz, "%04d-%02d-%02dT%02d:%02d:%02d,%02d,",
         year, month, day, hour, minute, second, gps.satellites());
@@ -141,6 +137,8 @@ void loop() {
     log_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
     logfile.write(",");
     log_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 7, 2);
+    
+    // Write out the tilt data and flush the write buffer
     logfile.write(tiltBuffer);
     logfile.flush();
     writeTilt = false;
@@ -148,8 +146,33 @@ void loop() {
   
 }
 
-static void log_float(float val, float invalid, int len, int prec)
-{
+void start_new_logfile(int day, int month, int year, int hour){
+  logfile.close(); // Close the old logfile
+  char name[13];
+  getLogFileName(name,day,month,year,hour);
+  
+  // Prevent over-writing by adding to the sub-sequence set
+  for (uint8_t i = 0; i < 10; i++) {
+    name[7] = '0' + i;
+    if (! SD.exists(name)) {
+      break;
+    }
+  }
+  
+  // Open the new file and set the current hour to the new hour
+  logfile = SD.open(name, FILE_WRITE);
+  if( ! logfile ) {
+    //error(1);
+    int dummy = 0;
+  }
+  current_hour = hour;
+  logfile.write("YYYY-MM-DDTHH:MM:SS,NUM_SATELLITES,LATITUDE,LONGITUDE,ALTITUDE,X_TILT,Y_TILT,TEMPERATURE,SERIAL_NUMBER\n");
+}
+
+static void log_float(float val, float invalid, int len, int prec) {
+  /*
+  Logs a float value
+  */
   if (val == invalid)
   {
     while (len-- > 1)
@@ -165,62 +188,70 @@ static void log_float(float val, float invalid, int len, int prec)
 }
 
 char getLogFileName(char *filename, int day, int month, int year, int hour) {
-/*
-Format is DDDYYHHA.DAT
-DDD - Day of year
-YY  - Year
-NN  - Logger unit number
-A - Sub-sequence name, used if more than one file/day
-*/
-strcpy(filename, "");
-strcpy(filename, "00000000.TXT");
-
-// Get day of year and set it
-int doy = calculateDayOfYear(day, month, year);
-
-filename[0] = '0' + (doy/100)%100;
-filename[1] = '0' + (doy/10)%10;
-filename[2] = '0' + doy%10;
-
-// Set the year
-filename[3] = '0' + (year/10)%10;
-filename[4] = '0' + year%10;
-
-// Set the hour
-if (hour != 0){
-  filename[5] = '0' + (hour/10)%10;
-  filename[6] = '0' + hour%10;
-}
-
+  /*
+  Format is DDDYYHHA.DAT
+  DDD - Day of year
+  YY  - Year
+  NN  - Logger unit number
+  A - Sub-sequence name, used if more than one file/day
+  */
+  strcpy(filename, "");
+  strcpy(filename, "00000000.TXT");
+  
+  // Get day of year and set it
+  int doy = calculateDayOfYear(day, month, year);
+  filename[0] = '0' + (doy/100)%100;
+  filename[1] = '0' + (doy/10)%10;
+  filename[2] = '0' + doy%10;
+  
+  // Set the year
+  filename[3] = '0' + (year/10)%10;
+  filename[4] = '0' + year%10;
+  
+  // Set the hour
+  if (hour != 0){
+    filename[5] = '0' + (hour/10)%10;
+    filename[6] = '0' + hour%10;
+  }
 }
 
 int calculateDayOfYear(int day, int month, int year) {
- /*
- Given a day, month, and year (2 or 4 digit), returns
- the day of year. Errors return 999.
- */
- // List of days in a month for a normal year
- int daysInMonth[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+  // Given a day, month, and year (4 digit), returns 
+  // the day of year. Errors return 999.
+  
+  int daysInMonth[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+  
+  // Verify we got a 4-digit year
+  if (year < 1000) {
+    return 999;
+  }
+  
+  // Check if it is a leap year, this is confusing business
+  // See: https://support.microsoft.com/en-us/kb/214019
+  if (year%4  == 0) {
+    if (year%100 != 0) {
+      daysInMonth[1] = 29;
+    }
+    else {
+      if (year%400 == 0) {
+        daysInMonth[1] = 29;
+      }
+    }
+   }
 
- // Check if it is a leap year
- if (year%4  == 0)
- {
-   daysInMonth[1] = 29;
- }
-
- // Make sure we are on a valid day of the month
- if (day < 1)
- {
-   return 999;
- } else if (day > daysInMonth[month-1]) {
-   return 999;
- }
-
- int doy = 0;
- for (int i = 0; i < month - 1; i++) {
-   doy += daysInMonth[i];
- }
-
- doy += day;
- return doy;
+  // Make sure we are on a valid day of the month
+  if (day < 1) 
+  {
+    return 999;
+  } else if (day > daysInMonth[month-1]) {
+    return 999;
+  }
+  
+  int doy = 0;
+  for (int i = 0; i < month - 1; i++) {
+    doy += daysInMonth[i];
+  }
+  
+  doy += day;
+  return doy;
 }
